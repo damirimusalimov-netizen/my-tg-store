892986b94d49ed99dc272e1a3bb7341e997b2af8
damirimusalimov-netizen <damir.i.musalimov@gmail.com>
2025-10-07 16:45:53 +0300

    3333333333333

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.Telegram && window.Telegram.WebApp) {
+    alert("✅ Telegram WebApp API найден!");
+    window.Telegram.WebApp.ready();
+  } else {
+    alert("❌ Telegram WebApp API не найден");
+  }
+});
